42c0c8c6ab594a3e41e2c0c93dfebf503a85d9b2
Phi Pham <phi.pham@fundingsocieties.com>
2023-08-25 10:19:48 +0700

    update karate-config

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,6 +1,6 @@
 function fn() {
     const config = {
-        baseUrl: 'https://service-mesh-ingress-{env}.fundingasiagroup.com/neobank-partner-integration-service/v1'
+        baseUrl: 'https://service-mesh-ingress-uat.fundingasiagroup.com/neobank-partner-integration-service/v1'
     };
 
     return config;
